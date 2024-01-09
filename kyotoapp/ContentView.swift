d99c619d606122bf57143d3a1b38eaca8aa7082f
Yuma Kurokawa <119155669+ultrabumbuku@users.noreply.github.com>
2024-01-09 15:12:11 +0900

    Initial Commit

---
+++ kyotoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kyotoapp
+//
+//  Created by 黒川悠馬 on 2024/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kyotoapp/kyotoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  kyotoappApp.swift
+//  kyotoapp
+//
+//  Created by 黒川悠馬 on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct kyotoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
